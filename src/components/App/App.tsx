df8d2dbda4d3eb5145c770321435e2942daf198f
Conchi Rodríguez Triviño <conchirodrigueztrivino@MacBook-Air-de-Conchi.local>
2023-07-11 11:28:07 +0200

    install daisy UI et test première intégration d'un composant. NavBar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import NavBar from "./NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="p-10 bg-red-600">blop</h1>
-  </div>
+<NavBar/>
   );
 }
 
