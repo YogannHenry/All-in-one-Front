d2d7fa6fb421efb283b68831f199d6773f705f73
Martins-Jarod <jarod.mrts@gmail.com>
2023-07-11 16:26:04 +0200

    ajout css

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,22 +3,28 @@ import Carrousel from './Carrousel/Carrousel';
 function Home() {
   return (
     <div>
-      <form>
-        <h1>All-in-One</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum is simply dummy text of the printing Lorem Ipsum
-          is simply dummy te printing and typesetting industry. Lorem and
-          typesetting industry
-        </p>
-        <input
-          type="text"
-          placeholder="Entrez votre email..."
-          className="input input-bordered w-full max-w-xs"
-        />
-        <a className="btn text-white bg-red-300 hover:bg-red-400">s'inscrire</a>
-        <Carrousel></Carrousel>
-      </form>
+      <div className="flex justify-center items-center">
+        <form className="">
+          <h1 className="text-center	">All-in-One</h1>
+          <p className="text-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum is simply dummy text of the printing Lorem
+            Ipsum is simply dummy te printing and typesetting industry. Lorem
+            and typesetting industry
+          </p>
+          <div className="flex justify-center items-center">
+            <input
+              type="text"
+              placeholder="Entrez votre email..."
+              className="input input-bordered w-full max-w-xs"
+            />
+            <a className="btn text-white bg-red-300 hover:bg-red-400">
+              s'inscrire
+            </a>
+          </div>
+        </form>
+      </div>
+      <Carrousel></Carrousel>
     </div>
   );
 }
