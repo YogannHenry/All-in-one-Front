f564d8faf01fd4ef3c178d7da4a482ac5578a66e
Martins-Jarod <jarod.mrts@gmail.com>
2023-07-11 15:31:25 +0200

    pour yogann

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,22 @@ import Carrousel from './Carrousel/Carrousel';
 function Home() {
   return (
     <div>
-      <Carrousel></Carrousel>
+      <form>
+        <h1>All-in-One</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum is simply dummy text of the printing Lorem Ipsum
+          is simply dummy te printing and typesetting industry. Lorem and
+          typesetting industry
+        </p>
+        <input
+          type="text"
+          placeholder="Entrez votre email..."
+          className="input input-bordered w-full max-w-xs"
+        />
+        <a className="btn text-white bg-red-300 hover:bg-red-400">s'inscrire</a>
+        <Carrousel></Carrousel>
+      </form>
     </div>
   );
 }
