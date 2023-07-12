849756a8a8d7670ae95440a9a6fa60a5bef9f325
YogannHenry <yogann.henry@gmail.com>
2023-07-12 11:01:27 +0200

    creation menu

---
+++ src/components/Partials/NavBar/DrawerButton/TodoListMenu/TodoListMenu.tsx
@@ -0,0 +1,20 @@
+function TodoListMenu() {
+  return (
+    <ul className="flex flex-col items-center">
+      <li>
+        <a className="font-bold underline uppercase text-base">Todo-List</a>
+      </li>
+      <li>
+        <a>List 1</a>
+      </li>
+      <li>
+        <a>List 2</a>
+      </li>
+      <li>
+        <a>List 3</a>
+      </li>
+    </ul>
+  );
+}
+
+export default TodoListMenu;
