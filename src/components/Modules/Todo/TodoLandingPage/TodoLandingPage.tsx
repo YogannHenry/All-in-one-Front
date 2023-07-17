2d7284b8e6c971d6e6d380d5b7a014193c04a4c1
Martins-Jarod <jarod.mrts@gmail.com>
2023-07-17 14:30:07 +0200

    ajout bouton suprression

--- src/components/Modules/Todo/TodoLandingPage/TodoLandingPage.tsx
+++ src/components/Modules/Todo/TodoLandingPage/TodoLandingPage.tsx
@@ -53,21 +53,39 @@ function TodoList() {
         <div className="card max-w-full w-1/2 bg-base-100 shadow-xl">
           <div className="card-body">
             {TodoListData.map((item) => (
-              <div className="collapse bg-base-200" key={item.id}>
-                <input type="radio" name="my-accordion-1" />
-                <div className="collapse-title text-xl font-medium">
-                  {item.Titre}
+              <div className="flex bg-base-200">
+                <div className="collapse bg-base-200" key={item.id}>
+                  <input type="radio" name="my-accordion-1" />
+                  <div className="collapse-title text-xl font-medium">
+                    {item.Titre}
+                  </div>
+                  <div className="collapse-content">
+                    <p>- {item.Task}</p>
+                  </div>
                 </div>
-                <div className="collapse-content">
-                  <p>- {item.Task}</p>
-                </div>
-                <div className="flex justify-center">
+                <div className="flex justify-center p-2 items-center">
                   <NavLink
                     to={`/list/${item.id}`}
                     className="text-blue-500 underline"
                   >
                     Ouvrir
                   </NavLink>
+                  <button className="btn bg-red-300 hover:bg-red-500 text-white ml-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
             ))}
