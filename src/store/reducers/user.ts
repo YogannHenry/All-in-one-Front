a7f73e188b4200dc81b3fa37f3ca81044d01b230
Martins-Jarod <jarod.mrts@gmail.com>
2023-07-26 14:49:51 +0200

    stocker token local storage

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -13,7 +13,7 @@ import axiosInstance from '../../utils/axios';
 interface UserState {
   logged: boolean;
   pseudo: string | null;
-  userId : number | null;
+  userId: number | null;
 }
 export const initialState: UserState = {
   logged: false,
@@ -27,23 +27,23 @@ export const logout = createAction('/logout');
 // Thunk pour la connexion
 export const login = createAsyncThunk('/login', async (formData: FormData) => {
   const objData = Object.fromEntries(formData);
-  console.log("objData2", objData);
+  console.log('objData2', objData);
 
   const { data } = await axiosInstance.post('/login', objData);
-  console.log("data",data);
+  console.log('data', data);
 
-    // après m'être connecté, j'ajoute mon token directement
-    // dans l'instance Axios
-    axiosInstance.defaults.headers.common.Authorization = `Bearer ${data.token}`;
-
-    // le token est utilisé ci-dessus, je n'en ai plus besoin
-    // je le supprime de mes données
-    delete data.token;
+  // après m'être connecté, j'ajoute mon token directement
+  // dans l'instance Axios
+  axiosInstance.defaults.headers.common.Authorization = `Bearer ${data.token}`;
+  localStorage.setItem('token', data.token);
+  // le token est utilisé ci-dessus, je n'en ai plus besoin
+  // je le supprime de mes données
+  delete data.token;
 
   return data as {
     logged: boolean;
     pseudo: string;
-    userId:number
+    userId: number;
   };
 });
 
@@ -56,7 +56,7 @@ const userReducer = createReducer(initialState, (builder) => {
       state.logged = action.payload.logged;
       state.pseudo = action.payload.pseudo;
       state.userId = action.payload.userId;
-      console.log("action.payload",action.payload);
+      console.log('action.payload', action.payload);
     })
     .addCase(logout, (state) => {
       // je ré-initialise mes données depuis mon state initial
@@ -65,6 +65,8 @@ const userReducer = createReducer(initialState, (builder) => {
 
       // à la déconnexion, je supprime le JWT de mon instance Axios
       delete axiosInstance.defaults.headers.common.Authorization;
+
+      localStorage.removeItem('token');
     });
 });
 
