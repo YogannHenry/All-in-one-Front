86569edc0d4a79afc43b29deac821eccf0333054
YogannHenry <yogann.henry@gmail.com>
2024-07-07 18:45:43 +0200

    Refactor file path handling in WalletDocumentsPage.tsx

--- src/components/Modules/Wallet/WalletDocumentsPage/WalletDocumentsPage.tsx
+++ src/components/Modules/Wallet/WalletDocumentsPage/WalletDocumentsPage.tsx
@@ -129,26 +129,31 @@ function WalletDocumentsPage() {
         (document) => document.id === documentId
       );
       console.log('documentData:', documentData);
-      if (documentData && documentData.type.startsWith('image/')) {
-        const imagePath = `https://all-in-1.fr/dist/uploads/${documentData.file}`;
-        setisDocumentOpen(true);
-        setImageToShow(imagePath);
-        console.log('imagePath2:', imagePath);
-      } else if (documentData && documentData.type.startsWith('application/')) {
-        const documentPath = `https://all-in-1.fr/dist/uploads/${documentData.file}`;
-        console.log('documentPath:', documentPath);
-        setisDocumentOpen(true);
-        setDocumentToShow(documentPath);
-        console.log('imagePath//documentPath:', documentPath);
+
+      if (documentData) {
+        const basePath = 'https://all-in-1.fr/uploads/';
+        const filePath = `${basePath}${documentData.file}`;
+        console.log('filePath:', filePath);
+
+        if (documentData.type.startsWith('image/')) {
+          setisDocumentOpen(true);
+          setImageToShow(filePath);
+          console.log('imagePath:', filePath);
+        } else if (documentData.type.startsWith('application/')) {
+          setisDocumentOpen(true);
+          setDocumentToShow(filePath);
+          console.log('documentPath:', filePath);
+        } else {
+          console.error(
+            'Type de fichier non pris en charge :',
+            documentData.type
+          );
+        }
       } else {
-        console.error(
-          'Type de fichier non pris en charge :',
-          documentData?.type
-        );
-        return;
+        console.error("Document non trouvé pour l'ID :", documentId);
       }
     } catch (error) {
-      console.error("Erreur lors de l'ouverture de l'image :", error);
+      console.error("Erreur lors de l'ouverture du document :", error);
     }
   };
 
